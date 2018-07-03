9f1f6d7025e5f2aa6a7f8be3da25656c993b1ec5
Oliver Wuenschel <oliver.wuenschel@gmail.com>
2018-07-03 02:31:21 +0200

    wip

--- main.py
+++ main.py
@@ -11,34 +11,49 @@ import schedule
 import time
 import threading
 from modules import SimpleHCServer
+from filelock import Timeout, FileLock
+
 
 my_port = 9001
 host = '127.0.0.1'
 hc_port = 50007
 
 #Provide Simple Healthcheck as own thread
-SimpleHCServer.get_lock(host, hc_port)
-threading.Thread(target=SimpleHCServer.run(host, my_port, hc_port)).start()
+#SimpleHCServer.get_lock(host, hc_port)
+hc_server = threading.Thread(target=SimpleHCServer.run(host, my_port))
+hc_server.setDaemon(True)
+hc_server.start()
+
+sys.exit()
+
+
+#time.sleep(10)
+
+
 
 #get_lock(my_srv_adress, hc_port)
 
-def job():
-    print("Still working...")
+# def job():
+#     print("Still working...")
 #    print(bck.name)
 
 #schedule.every().minutes.do(job)
 # schedule.every().monday.at("23:37").do(job)
 # schedule.every().wednesday.at("13:15").do(job)
-schedule.every(2).seconds.do(job)
+#schedule.every(2).seconds.do(job)
 
-def dobackup():
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+# def dobackup():
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
 
-dobackup()
+#dobackup()
 
-sleep(90)
+
+
+
+
+
 
 
 
--- modules/SimpleHCServer.py
+++ modules/SimpleHCServer.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
 import socket
 import sys
+from filelock import Timeout, FileLock
 
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler) :
 
@@ -9,62 +10,34 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler) :
     def do_GET(self):
         
         # Send response status code
-        if not self.get_connect(self.hc_host, self.hc_port):
-            print('Could not connect to {}:{}'.format(self.hc_host, self.hc_port))
-            #self.send_response(404, message='healthcheck failed')
-            self.send_error(404,
-                            message='Healthcheck failed', 
-                            explain='Could not connect to {}:{}'.format(self.hc_host, self.hc_port))
-        else:
-            # Send message back to client
-            message = 'connected to {}:{}'.format(self.hc_host, self.hc_port)
-            self.send_response(200)
-            # Send headers
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            # Write content as utf-8 data
-            self.wfile.write(bytes(message, "utf8"))
+        # if not mainlock.is_locked:
+        #     print('{} seems to be unlocked. terminate.'.format(mainlock.lock_file))
+        #     #self.send_response(404, message='healthcheck failed')
+        #     self.send_error(404,
+        #                     message='Healthcheck failed', 
+        #                     explain='{} seems to be unlocked. terminate.'.format(mainlock.lock_file))
+        # else:
+        #Send message back to client
+        message = str(threading.main_thread().is_alive())
+        self.send_response(200)
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
             
-
-        
         return
 
-    def get_connect(self, host, port):
-        get_connect_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       
-        try:
-            get_connect_sock.connect((host, port))
-            print('connected')
-        except socket.error:
-            print(host, port)
-            print('cant connect')
-            sys.exit()
-        finally:
-            print('closing connection')
-            get_connect_sock.close()
-        
-        return True
 
-
-
-def get_lock(host, port):
-    get_lock._lock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        get_lock._lock_socket.bind((host, port))
-        get_lock._lock_socket.listen(5)
-        print('I got the lock')
-    except socket.error:
-        print('lock exists')
-        sys.exit()
-
-def run(my_srv_adress, my_port, hc_port):
+def run(my_srv_adress, my_port):
     print("Starting Server...")
+    print(threading.main_thread)
+    print(threading.main_thread().is_alive())
     server_address = (my_srv_adress, my_port)
-    testHTTPServer_RequestHandler.hc_port = hc_port
-    testHTTPServer_RequestHandler.hc_host = my_srv_adress
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-    httpd.serve_forever()
+    httpd.serve_forever(poll_interval=5)
+    
+
     
 
     
